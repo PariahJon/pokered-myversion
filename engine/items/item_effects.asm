4e4832502aa085efd50cc3fd589af9cee161fc49
PariahJon <88702621+PariahJon@users.noreply.github.com>
2025-10-19 16:58:48 -0400

    Fix: PP restoring items do not account for PP Ups when used

--- engine/items/item_effects.asm
+++ engine/items/item_effects.asm
@@ -2081,6 +2081,9 @@ ItemUsePPRestore:
 ; how many PP Ups have been used on the move.
 ; So, Max Ethers and Max Elixirs will not be detected as having no effect on
 ; a move with full PP if the move has had any PP Ups used on it.
+
+	and %00111111 ; lower 6 bits store current PP
+
 	cp b ; does current PP equal max PP?
 	ret z
 	jr .storeNewAmount
