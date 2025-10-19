c2ad62746601d1534466186ecf00b572a3803c66
PariahJon <88702621+PariahJon@users.noreply.github.com>
2025-10-19 18:00:23 -0400

    Fix: Struggle may not function correctly if any move has at least one PP Up

--- engine/battle/core.asm
+++ engine/battle/core.asm
@@ -2747,7 +2747,10 @@ AnyMoveToSelect:
 	or c
 	jr .handleDisabledMovePPLoop
 .allMovesChecked
-	and a ; any PP left?
+;	and a ; any PP left?
+
+	and $3f ; any PP left?
+
 	ret nz ; return if a move has PP left
 .noMovesLeft
 	ld hl, NoMovesLeftText
