85925bff0a2de50e855fa572dff3e956d564ef86
PariahJon <88702621+PariahJon@users.noreply.github.com>
2025-10-19 18:46:08 -0400

    Fix: Bicycle clerk causes text to appear instantly

--- scripts/BikeShop.asm
+++ scripts/BikeShop.asm
@@ -63,11 +63,15 @@ BikeShopClerkText:
 	call PlaceString
 	ld hl, BikeShopClerkDoYouLikeItText
 	call PrintText
+
+	ld hl, wStatusFlags5
+	res BIT_NO_TEXT_DELAY, [hl]
+
 	call HandleMenuInput
 	bit B_PAD_B, a
 	jr nz, .cancel
-	ld hl, wStatusFlags5
-	res BIT_NO_TEXT_DELAY, [hl]
+;	ld hl, wStatusFlags5
+;	res BIT_NO_TEXT_DELAY, [hl]
 	ld a, [wCurrentMenuItem]
 	and a
 	jr nz, .cancel
