61b5c774c189c714010423c725295a01fce64c74
PariahJon <88702621+PariahJon@users.noreply.github.com>
2025-10-20 00:47:08 -0400

    Fix: Disable fishing and surfing in statues

--- engine/items/item_effects.asm
+++ engine/items/item_effects.asm
@@ -2832,17 +2832,35 @@ IsNextTileShoreOrWater:
 	ld de, 1
 	call IsInArray
 	jr nc, .notShoreOrWater
+
+	ld hl, WaterTile
+
 	ld a, [wCurMapTileset]
 	cp SHIP_PORT ; Vermilion Dock tileset
-	ld a, [wTileInFrontOfPlayer] ; tile in front of player
+;	ld a, [wTileInFrontOfPlayer] ; tile in front of player
+
 	jr z, .skipShoreTiles ; if it's the Vermilion Dock tileset
-	cp $48 ; eastern shore tile in Safari Zone
-	jr z, .shoreOrWater
-	cp $32 ; usual eastern shore tile
-	jr z, .shoreOrWater
+	cp GYM ; eastern shore tile in Safari Zone
+	jr z, .skipShoreTiles
+	cp DOJO ; usual eastern shore tile
+
+	jr z, .skipShoreTiles ; if it's the Vermilion Dock tileset
+;	cp $48 ; eastern shore tile in Safari Zone
+;	jr z, .shoreOrWater
+;	cp $32 ; usual eastern shore tile
+;	jr z, .shoreOrWater
+
+	ld hl, ShoreTiles
+
 .skipShoreTiles
-	cp $14 ; water tile
-	jr z, .shoreOrWater
+;	cp $14 ; water tile
+;	jr z, .shoreOrWater
+
+	ld a, [wTileInFrontOfPlayer]
+	ld de, $1
+	call IsInArray
+	jr c, .shoreOrWater
+
 .notShoreOrWater
 	scf
 	ret
@@ -2850,7 +2868,19 @@ IsNextTileShoreOrWater:
 	and a
 	ret
 
-INCLUDE "data/tilesets/water_tilesets.asm"
+; shore tiles
+ShoreTiles:
+	db $48, $32
+WaterTile:
+	db $14
+	db $ff ; terminator
+
+; tilesets with water
+WaterTilesets:
+	db OVERWORLD, FOREST, DOJO, GYM, SHIP, SHIP_PORT, CAVERN, FACILITY, PLATEAU
+	db $ff ; terminator
+
+;INCLUDE "data/tilesets/water_tilesets.asm"
 
 ReadSuperRodData:
 ; return e = 2 if no fish on this map
