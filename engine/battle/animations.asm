ac1df69424c00b9905a7cb9f472905a3a53d4796
PariahJon <88702621+PariahJon@users.noreply.github.com>
2025-10-20 00:54:44 -0400

    Fix: The lower-right tile of Pokémon backsprites are deleted when sliding offscreen

--- engine/battle/animations.asm
+++ engine/battle/animations.asm
@@ -1839,7 +1839,10 @@ _AnimationSlideMonOff:
 ; This is a bug. The lower right corner tile of the mon back pic is blanked
 ; while the mon is sliding off the screen. It should compare with the max tile
 ; plus one instead.
-	cp $61
+;	cp $61
+
+	cp $62
+
 	ret c
 	ld a, " "
 	ret
@@ -1849,7 +1852,10 @@ _AnimationSlideMonOff:
 	sub 7
 ; This has the same problem as above, but it has no visible effect because
 ; the lower right tile is in the first column to slide off the screen.
-	cp $30
+;	cp $30
+
+	cp $31
+
 	ret c
 	ld a, " "
 	ret
