b4cb46e6c6ca61a66a35b1ad6071dc813eb7606b
PariahJon <88702621+PariahJon@users.noreply.github.com>
2025-10-22 01:31:36 -0400

    Updated text in data\battle

--- data/battle/stat_names.asm
+++ data/battle/stat_names.asm
@@ -2,9 +2,16 @@
 
 VitaminStats:
 	list_start
-	li "HEALTH"
-	li "ATTACK"
-	li "DEFENSE"
-	li "SPEED"
-	li "SPECIAL"
+;	li "HEALTH"
+;	li "ATTACK"
+;	li "DEFENSE"
+;	li "SPEED"
+;	li "SPECIAL"
+
+	li "Health"
+	li "Attack"
+	li "Defense"
+	li "Speed"
+	li "Special"
+
 	assert_list_length NUM_STATS
